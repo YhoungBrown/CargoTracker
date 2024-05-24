f0b853f1c9d8e3ccb155eee4c12a5f102a7d2b07
Omotola <omotolaodumosu04@gmail.com>
2024-05-24 03:44:39 +0100

    delivery card almost finished. everything is good so far

--- components/DeliveryCard.tsx
+++ components/DeliveryCard.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Card, Icon } from '@rneui/themed'
 
  type prop = {
   order: Order
@@ -7,9 +8,63 @@ import React from 'react'
 
 const DeliveryCard = ({order}: prop) => {
   return (
-    <View>
-      <Text>DeliveryCard</Text>
+    <Card containerStyle={{
+      borderRadius: 8, 
+      marginVertical: 8, 
+      backgroundColor: "#59C1CC", 
+      padding:0, 
+      paddingTop: 16, 
+      shadowColor: "black", 
+      shadowOffset: {width: 0, height: 2}, 
+      shadowOpacity: 0.2, 
+      shadowRadius: 4}}>
+      <View>
+        <Icon name='box' type='entypo' color={"white"} size={50}/>
+
+        <View style={{alignItems: "center", justifyContent: 'center'}}>
+          <Text style={{
+            fontSize: 14, 
+            lineHeight: 20, 
+            textTransform: 'uppercase', 
+            color: "white", 
+            fontWeight: 'bold'}}>
+              {order.carrier} - {order.trackingId}
+          </Text>
+
+          <Text style={{color: "white", fontSize: 18, lineHeight: 20, fontWeight:'bold'}}> 
+            Expected Delivery: {new Date(order.createdAt).toLocaleDateString()}
+          </Text>
+
+          <View style={{backgroundColor: "white", height: 0.5, width: "100%", marginVertical: 10}}/>
+
+          <View>
+            <Text style={{textAlign: 'center', color: "white", fontWeight: 'bold', marginTop: 5 , fontSize: 15, lineHeight: 24}}>
+              Address: 
+            </Text>
+            <Text style={{fontSize: 14, lineHeight: 20, textAlign: 'center', color: "white"}}>{order.Address}, {order.City}</Text>
+            <Text style={{fontSize: 14, lineHeight: 20, textAlign: 'center', color: "white", fontStyle: 'italic'}}>Shipping Cost: €{order.shippingCost}</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={{backgroundColor: "white", height: 0.5, width: "100%", marginVertical: 10}}/>
+
+    <View style={{padding: 20}}>
+
+      {order.trackingItems.items.map(item => (
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+          
+            <Text style={{fontSize: 14, lineHeight: 20, textAlign: 'center', color: "white", fontStyle: 'italic'}}>
+              {item.name}
+            </Text>
+            <Text style={{fontSize: 20, lineHeight: 28, textAlign: 'center', color: "white", fontStyle: 'italic'}}>
+              x{item.quantity}
+            </Text>
+          
+        </View>
+      ))}
     </View>
+    </Card>
   )
 }
 
